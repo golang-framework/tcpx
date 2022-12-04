bc05452aa60e06741b9e844325f330b25f4d04da
yuwenyu <910482444@qq.com>
2022-12-05 00:19:11 +0800

    d

--- znet/connection.go
+++ znet/connection.go
@@ -6,6 +6,7 @@ import (
 	"encoding/hex"
 	"errors"
 	"fmt"
+	"io"
 	"net"
 	"sync"
 	"time"
@@ -37,6 +38,8 @@ type Connection struct {
 	propertyLock sync.Mutex
 	//当前连接的关闭状态
 	isClosed bool
+
+	buf bytes.Buffer
 }
 
 // NewConnection 创建连接的方法
@@ -117,60 +120,62 @@ func (c *Connection) StartReader() {
 			// - 2e				[BCC 校验码]
 			//-<=======================================================================
 
-			bufSource := make([]byte, 1024)
-			numSource, errSource := c.Conn.Read(bufSource)
-			if errSource != nil {
-				return
-			}
-
-			arrSource := bytes.Split(bufSource[:numSource], []byte{0x7e})
-			if len(arrSource) < 3 {
-				break
-			}
-
-			startNum := 0
-			if len(arrSource[startNum]) != 0 {
-				arrSource = arrSource[1:]
-			}
-
-			endedNum := len(arrSource) - 1
-			if len(arrSource[endedNum]) != 0 {
-				arrSource = arrSource[:endedNum-1]
-			}
-
-			if len(arrSource) <= 0 {
-				break
-			}
-
-			res := make([][]byte, 0)
-
-			for _, src := range arrSource {
-				if len(src) != 0 {
-					res = append(res, src)
-				}
-			}
-
-			if len(res) < 1 {
-				break
-			}
-
-			for _, v := range res {
-				if bytes.Contains(v, []byte{0x7d, 0x02}) {
-					v = bytes.Replace(v, []byte{0x7d, 0x02}, []byte{0x7e}, -1)
-				}
-
-				if bytes.Contains(v, []byte{0x7d, 0x01}) {
-					v = bytes.Replace(v, []byte{0x7d, 0x01}, []byte{0x7d}, -1)
-				}
-
-				c.SendReqToTaskQueue(c.MsgType(hex.EncodeToString(v[:2])), v, uint32(len(v)))
-			}
-
-			//bufSource := make([]byte, 32)
-			//_, errSource := io.ReadFull(c.Conn, bufSource)
+			// -
+			//bufSource := make([]byte, 1024)
+			//numSource, errSource := c.Conn.Read(bufSource)
 			//if errSource != nil {
 			//	return
 			//}
+			//
+			//arrSource := bytes.Split(bufSource[:numSource], []byte{0x7e})
+			//if len(arrSource) < 3 {
+			//	break
+			//}
+			//
+			//startNum := 0
+			//if len(arrSource[startNum]) != 0 {
+			//	arrSource = arrSource[1:]
+			//}
+			//
+			//endedNum := len(arrSource) - 1
+			//if len(arrSource[endedNum]) != 0 {
+			//	arrSource = arrSource[:endedNum-1]
+			//}
+			//
+			//if len(arrSource) <= 0 {
+			//	break
+			//}
+			//
+			//res := make([][]byte, 0)
+			//
+			//for _, src := range arrSource {
+			//	if len(src) != 0 {
+			//		res = append(res, src)
+			//	}
+			//}
+			//
+			//if len(res) < 1 {
+			//	break
+			//}
+			//
+			//for _, v := range res {
+			//	if bytes.Contains(v, []byte{0x7d, 0x02}) {
+			//		v = bytes.Replace(v, []byte{0x7d, 0x02}, []byte{0x7e}, -1)
+			//	}
+			//
+			//	if bytes.Contains(v, []byte{0x7d, 0x01}) {
+			//		v = bytes.Replace(v, []byte{0x7d, 0x01}, []byte{0x7d}, -1)
+			//	}
+			//
+			//	c.SendReqToTaskQueue(c.MsgType(hex.EncodeToString(v[:2])), v, uint32(len(v)))
+			//}
+			// -
+
+			bufSource := make([]byte, 32)
+			_, errSource := io.ReadFull(c.Conn, bufSource)
+			if errSource != nil {
+				return
+			}
 
 			//bufSource := make([]byte, 32)
 			//_, errSource := c.Conn.Read(bufSource)
@@ -178,30 +183,31 @@ func (c *Connection) StartReader() {
 			//	return
 			//}
 
-			//if len(bufSource) < 1 {
-			//	break
-			//}
-			//
-			//for _, v := range bufSource {
-			//	if bytes.Equal([]byte{v}, []byte{0x7e}) {
-			//		if buf.Len() > 0 {
-			//			res := buf.Bytes()
-			//
-			//			if bytes.Contains(res, []byte{0x7d, 0x02}) {
-			//				res = bytes.Replace(res, []byte{0x7d, 0x02}, []byte{0x7e}, -1)
-			//			}
-			//
-			//			if bytes.Contains(res, []byte{0x7d, 0x01}) {
-			//				res = bytes.Replace(res, []byte{0x7d, 0x01}, []byte{0x7d}, -1)
-			//			}
-			//
-			//			buf.Reset()
-			//			go c.SendReqToTaskQueue(c.MsgType(hex.EncodeToString(res[:2])), res, uint32(len(res)))
-			//		}
-			//	} else {
-			//		buf.Write([]byte{v})
-			//	}
-			//}
+			if len(bufSource) < 1 {
+				break
+			}
+
+			for _, v := range bufSource {
+				if bytes.Equal([]byte{v}, []byte{0x7e}) {
+					if c.buf.Len() > 0 {
+						res := c.buf.Bytes()
+
+						if bytes.Contains(res, []byte{0x7d, 0x02}) {
+							res = bytes.Replace(res, []byte{0x7d, 0x02}, []byte{0x7e}, -1)
+						}
+
+						if bytes.Contains(res, []byte{0x7d, 0x01}) {
+							res = bytes.Replace(res, []byte{0x7d, 0x01}, []byte{0x7d}, -1)
+						}
+
+						c.buf.Reset()
+						fmt.Println("--", hex.EncodeToString(res))
+						go c.SendReqToTaskQueue(c.MsgType(hex.EncodeToString(res[:2])), res, uint32(len(res)))
+					}
+				} else {
+					c.buf.Write([]byte{v})
+				}
+			}
 		}
 	}
 }
